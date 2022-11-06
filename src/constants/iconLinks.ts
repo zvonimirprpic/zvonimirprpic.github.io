532e15e587995060098d25ed5b94a01b5d64c5f1
unknown <zvonimirprpic@yahoo.com>
2022-11-06 21:16:40 +0100

    added titles to links

--- src/constants/iconLinks.ts
+++ src/constants/iconLinks.ts
@@ -2,6 +2,7 @@ interface IIconLink {
   href: string;
   opensNewTab: boolean;
   icon: string;
+  title: string;
 }
 
 export const iconLinks: IIconLink[] = [
@@ -9,15 +10,18 @@ export const iconLinks: IIconLink[] = [
     href: "https://github.com/zvonimirprpic",
     opensNewTab: true,
     icon: "githubIcon",
+    title: "GitHub Link",
   },
   {
     href: "https://www.linkedin.com/in/zvonimirprpic/",
     opensNewTab: true,
     icon: "linkedinIcon",
+    title: "LinkedIn Link,",
   },
   {
     href: "mailto:zvonimirprpic@yahoo.com",
     opensNewTab: false,
     icon: "emailIcon",
+    title: "E-Mail",
   },
 ];
